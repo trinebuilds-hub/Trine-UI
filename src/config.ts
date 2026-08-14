953248b1fe28fe18c9fd44041fd0c61946c2fe5c
navaneethgowda12 <navaneethgowdamp07@gmail.com>
2026-08-15 01:45:13 +0530

    made some minor changes

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const CONFIG = {
 
   EMAIL: "trineworksfx@gmail.com",
   PHONE: "8105734081",
-  PHONE_DISPLAY: "810-573-4091",
+  PHONE_DISPLAY: "8105734081",
   INSTAGRAM_HANDLE: "@trineworks_fx",
   INSTAGRAM_URL: "https://www.instagram.com/trineworks_fx",
 };
